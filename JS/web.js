d12b8edffdf932940e385a4f2fc47d6f1039b3ce
cr7st74n <cris-082011@hotmail.com>
2022-07-09 23:33:16 -0500

    getting and displaying information of the API on console

--- JS/web.js
+++ JS/web.js
@@ -7,23 +7,29 @@ var Number = "5"
 var keyCR = "3b7d870a84d61ceaae66a2fbe9363f4a"
 
 function DataCity(){
-     var city= $("#icon_prefix").val();
-     console.log(city);
+
+     $("#startSearch").on("click",function(hey){
+          var city= $("#icon_prefix").val();
+          console.log('yes');
+          Weather_app(city);
+     })
 }
 
-function Weather_app(){
-
+function Weather_app(city){
 
      var url = (`http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=${Number}&appid=${keyCR}`);
 
      var climateAPI = fetch(url).then(function(ResposeWeather){
           var WeatherData = ResposeWeather.json();
-          console.log(WeatherData);
-
-     });
-
-
-
+          return WeatherData;
+     })
+     .then(function(ArrData){
+          console.log(ArrData[0]);
+     })
+     .catch(function(err){
+          console.log(err);
+     })
+     ;
 } 
 
-//Weather_app();
+console.log(DataCity());
