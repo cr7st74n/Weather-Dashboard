e2b2e7c0012586dc855f5117393a1d0fb236b981
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2022-07-11 12:55:09 -0400

    getting all the data from API

--- JS/web.js
+++ JS/web.js
@@ -5,12 +5,12 @@ console.log(mom);
 
 var Number = "5"
 var keyCR = "3b7d870a84d61ceaae66a2fbe9363f4a"
+var key2 = "c59053e9cfd68c4fcf70f81e077a7583"
 
 function DataCity(){
 
      $("#startSearch").on("click",function(hey){
           var city= $("#icon_prefix").val();
-          console.log('yes');
           Weather_app(city);
      })
 }
@@ -18,18 +18,53 @@ function DataCity(){
 function Weather_app(city){
 
      var url = (`http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=${Number}&appid=${keyCR}`);
+     var cityName;
+     var latCity;
+     var lonCity
 
      var climateAPI = fetch(url).then(function(ResposeWeather){
           var WeatherData = ResposeWeather.json();
           return WeatherData;
      })
      .then(function(ArrData){
-          console.log(ArrData[0]);
+          cityName = ArrData[0].name;
+          console.log(cityName);
+          latCity = ArrData[0].lat;
+          console.log(latCity);
+          lonCity = ArrData[0].lon;
+          console.log(lonCity);  
+
+          passData(latCity,lonCity);
      })
      .catch(function(err){
           console.log(err);
+          console.log('here is an error');
      })
      ;
 } 
+
+function passData(lat,long){
+
+     var url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${keyCR}`);
+
+     var DataWeather = fetch(url).then(function(ResponseWeb){
+          var WeatherData = ResponseWeb.json();
+          return WeatherData;
+     })
+     .then(function(conditions){
+          var temp = conditions;
+          console.log("adsfaf")
+          console.log(temp.main);
+
+     })
+     .catch(function(err){
+          console.log(err);
+          console.log('here is an error');
+     })
+
+}
+
+
+
 
 console.log(DataCity());
